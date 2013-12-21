f3923bf1f5db47e33ae23f4d956f624582338324
Jann Mueller <j.mueller.11@ucl.ac.uk>
2013-12-21 22:44:11 +0000

    Add some primitives

--- src/Data/Time/Patterns.hs
+++ src/Data/Time/Patterns.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE RecordWildCards #-}
 module Data.Time.Patterns where
 
-test :: String
-test = "Hello!"
+import Data.Time.Calendar (Day, addDays, addGregorianMonthsClip)
+
+-- | A pattern describing re-occuring events
+newtype DatePattern = DatePattern { nextOccurence :: Day -> Maybe Day }
+
+-- | A weekly event
+weekly :: DatePattern
+weekly = DatePattern{..} where
+  nextOccurence = Just . addDays 7
+
+-- | A monthly event. 
+monthly :: DatePattern
+monthly = DatePattern{..} where
+  nextOccurence = Just . addGregorianMonthsClip 1
+
+
