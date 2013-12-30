6dfd4753218775ae2f39bdfd141d04cb66f55c3c
Jann Mueller <j.mueller.11@ucl.ac.uk>
2013-12-30 11:40:48 +0000

    Implement conversion of DatePattern to TimePattern and the DatePattern for every day

--- src/Data/Time/Intervals.hs
+++ src/Data/Time/Intervals.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Time.Intervals
@@ -10,8 +11,64 @@
 module Data.Time.Intervals where
 
 import Numeric.Interval
-import Data.Thyme.Calendar (YearMonthDay)
-import Data.Thyme.Clock (UTCTime)
+import Control.Lens
+import Data.AdditiveGroup
+import Data.Thyme.Calendar (Day)
+import Data.Thyme.Clock (UTCTime, UTCView(..), _utctDay, utcTime)
 
-type DateInterval = Interval YearMonthDay
-type TimeInterval = Interval UTCTime
+-- if the argument to nextOccurrence is part of an interval, then the result should be the interval containing it.
+-- The interval should be closed at the first parameter and open at the second, so that repeated calls of
+-- nextOccurrence yield a sequence of occurrences.
+newtype IntervalSequence t = IntervalSequence { nextInterval :: t -> Maybe (Interval t, IntervalSequence t)}
+-- instance Functor IntervalSequence
+-- instance Foldable IntervalSequence
+-- instance Traversable IntervalSequence
+type DatePattern = IntervalSequence Day
+type TimePattern = IntervalSequence UTCTime -- TimePattern should cycle throu
+--type ZonedTimePattern = IntervalSequence ZonedTime
+
+
+--toTimeZone :: TimeZone -> TimePattern -> ZonedTimePattern
+
+days :: DatePattern
+days = IntervalSequence{..} where
+    nextInterval t = Just (I t (succ t), days)
+
+toUtcTime :: DatePattern -> TimePattern
+toUtcTime dp = IntervalSequence ni where
+    ni time = (ni' (day time)) >>= \r -> return (fmap dayToUtcTime $ fst r, toUtcTime $ snd r)
+    day t = t ^. _utctDay
+    ni' = nextInterval dp
+
+every :: Integer -> DatePattern -> DatePattern 
+every = undefined
+
+mondays :: DatePattern
+mondays = undefined
+
+intersect :: Num t => IntervalSequence t -> IntervalSequence t -> IntervalSequence t
+intersect = undefined
+
+minus :: Num t => IntervalSequence t -> IntervalSequence t -> IntervalSequence t 
+minus = undefined
+
+cycle :: t -> IntervalSequence t
+cycle = undefined
+
+elementOf :: t -> IntervalSequence t -> Bool
+elementOf = undefined
+
+occurrencesFrom :: t -> IntervalSequence t -> [t]
+occurrencesFrom = undefined
+
+-- TO DO: When easter can be implemented using the combinators, the library
+-- can be released.
+easter :: DatePattern
+easter = undefined
+
+-- Some helper functions
+
+dayToUtcTime :: Day -> UTCTime
+dayToUtcTime day = (UTCTime day midnight)^.from utcTime
+    where
+        midnight = zeroV
